82e9eb067368ce494893c2d482f8eb6a0c129808
Matt Weiden <538456+mweiden@users.noreply.github.com>
2020-04-14 12:20:56 -0700

    Fix typos and include next rotation

--- alarms/hosted_cellxgene/alarm.py
+++ alarms/hosted_cellxgene/alarm.py
@@ -3,12 +3,26 @@ from bronze.libs import alarm as bronze_alarm
 import pendulum
 
 
-def get_slack_mapping(name):
-    try:
-        slack_id = slack_user_mapping[name].tag
-    except KeyError:
-        slack_id = None
-    return slack_id
+def oncall_period(start_date):
+    return start_date.add(hours=10), start_date.add(days=7, hours=10).subtract(seconds=1)
+
+
+def get_rotation(df, date):
+    def get_slack_mapping(name):
+        try:
+            slack_id = slack_user_mapping[name].tag
+        except KeyError:
+            slack_id = None
+        return slack_id
+
+    rotation = df.loc[df['Start Date'] == date.format("MMM D, YYYY")]
+
+    # Extract and validate the date and coordinator for composing the messages
+    primary = bronze_alarm.load_cell_from_row(column_name='Primary', row=rotation)
+    secondary = bronze_alarm.load_cell_from_row(column_name='Secondary', row=rotation)
+    primary_slack_id = get_slack_mapping(primary)
+    secondary_slack_id = get_slack_mapping(secondary)
+    return primary_slack_id, secondary_slack_id
 
 
 # Create and initialize the alarm from the YAML file
@@ -24,41 +38,41 @@ slack_user_mapping = bronze_alarm.get_slack_mapping(
 )
 
 # Get the date of "today", which is the date of the script invocation time
-invocation_date = pendulum.today(tz="America/Los_Angeles")
-print(f"The current invocation date is {invocation_date}")
+current_rotation_date = pendulum.today(tz="America/Los_Angeles")
+next_rotation_date = current_rotation_date.add(days=7)
+print(f"The current rotation date is {current_rotation_date}")
+print(f"The next rotation date is {next_rotation_date}")
+
 
 # Look for tomorrow
-period_start_datetime = invocation_date.add(hours=10)
-period_end_datetime = period_start_datetime.subtract(seconds=1)
-
-invocation_date_str = invocation_date.format("MMM D, YYYY")
+current_start_datetime, current_end_datetime = oncall_period(current_rotation_date)
+next_start_datetime, next_end_datetime = oncall_period(next_rotation_date)
 
 # Figure out which target date in the rotation the invocation day matches
-print(f"Looking for exact matches of {invocation_date_str} in the rotation sheet...")
 target_dates_df = spreadsheet.sheetToDataFrame(
     sheet_name='Rotation', sheet_range='A1:C1000', has_header=True
 )
-target = target_dates_df.loc[target_dates_df['Start Date'] == invocation_date_str]
 
-# Extract and validate the date and coordinator for composing the messages
-date = bronze_alarm.load_cell_from_row(column_name='Start Date', row=target)
-primary = bronze_alarm.load_cell_from_row(column_name='Primary', row=target)
-secondary = bronze_alarm.load_cell_from_row(column_name='Secondary', row=target)
 
-primary_slack_id = get_slack_mapping(primary)
-secondary_slack_id = get_slack_mapping(secondary)
+current_primary_slack_id, current_secondary_slack_id = get_rotation(target_dates_df, current_rotation_date)
+next_primary_slack_id, next_secondary_slack_id = get_rotation(target_dates_df, next_rotation_date)
+
 
 # Compose the message while respecting the "skip" flags
-if not primary and not secondary:
-    msg = "There's nobody scheduled for on-call tomorrow! Please update the <rotation spreadsheet|https://docs.google.com/spreadsheets/d/13PC0kkNcXMC4_7UJRgjQ6cEXxl5D58mxJD20mAMGImo/edit#gid=1508723546>"
+if any(ele is None for ele in [current_primary_slack_id, current_secondary_slack_id, next_primary_slack_id, next_secondary_slack_id]):
+    msg = "There are people missing in the on-call rotation spreadsheet! Please update the <rotation spreadsheet|https://docs.google.com/spreadsheets/d/13PC0kkNcXMC4_7UJRgjQ6cEXxl5D58mxJD20mAMGImo/edit#gid=1508723546>"
 else:
     msg = oncall_reminder.message
     msg = msg.format(
         **{
-            "primary": primary_slack_id,
-            "secondary": secondary_slack_id,
-            "start_datetime": period_start_datetime.to_iso8601_string(),
-            "end_datetime": period_end_datetime.to_iso8601_string()
+            "current_primary": current_primary_slack_id,
+            "current_secondary": current_secondary_slack_id,
+            "next_primary": next_primary_slack_id,
+            "next_secondary": next_secondary_slack_id,
+            "current_start_datetime": current_start_datetime.to_iso8601_string(),
+            "current_end_datetime": current_end_datetime.to_iso8601_string(),
+            "next_start_datetime": next_start_datetime.to_iso8601_string(),
+            "next_end_datetime": next_end_datetime.to_iso8601_string()
         }
     )
 
